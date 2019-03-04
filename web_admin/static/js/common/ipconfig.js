b9ffe81fa7c5ed2f82b0cf3552bf41316e06bea3
Albert Vince Mendiola <vinzmendiola@gmail.com>
2019-03-04 10:53:43 +0800

    IP Config Settings

--- web_admin/static/js/common/ipconfig.js
+++ web_admin/static/js/common/ipconfig.js
@@ -1,5 +1,6 @@
 var app = angular.module('common_config');
 
 app.constant('configSettings', {
-	'baseUrl' : 'http://192.168.0.109:8000/api/'
+	// 'baseUrl' : 'http://192.168.0.109:8000/api/'
+	'baseUrl' : 'https://yahshuaintelex.com/api/'
 });
