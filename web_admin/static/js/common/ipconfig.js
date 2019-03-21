f5686e05ebf61c70193ceaa21a31301b24acd949
Albert Vince Mendiola <vinzmendiola@gmail.com>
2019-03-21 16:32:34 +0800

    IPCOnfig for admin

--- web_admin/static/js/common/ipconfig.js
+++ web_admin/static/js/common/ipconfig.js
@@ -1,6 +1,6 @@
 var app = angular.module('common_config');
 
 app.constant('configSettings', {
-	'baseUrl' : 'http://127.0.0.1:8000/api/'
-	// 'baseUrl' : 'https://yahshuaintelex.com/api/'
+	// 'baseUrl' : 'http://127.0.0.1:8000/api/'
+	'baseUrl' : 'https://yahshuaintelex.com/api/'
 });
