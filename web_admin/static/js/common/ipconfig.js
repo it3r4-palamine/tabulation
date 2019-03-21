6fad36833b81c9f540c5ddb06311e5c9bb97b091
Albert Vince Mendiola <vinzmendiola@gmail.com>
2019-03-21 17:28:24 +0800

    Ignore IPConfig

--- web_admin/static/js/common/ipconfig.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('common_config');
-
-app.constant('configSettings', {
-	// 'baseUrl' : 'http://127.0.0.1:8000/api/'
-	'baseUrl' : 'https://yahshuaintelex.com/api/'
-});
--- web_student/static/web_student/js/common/ipconfig.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('common_config', []);
-
-app.constant('configSettings', {
-	// 'baseUrl' : 'http://192.168.0.109:8000/api/'
-	'baseUrl' : 'https://yahshuaintelex.com/api/'
-	// 'baseUrl' : 'http://127.0.0.1:8000/api/'
-});
