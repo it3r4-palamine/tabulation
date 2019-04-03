62a41930aa14a992b6b5860725dc2827c0f1acd0
Albert Vince Mendiola <vinzmendiola@gmail.com>
2019-04-03 11:32:41 +0800

    Additional

--- api/admin.py
+++ api/admin.py
@@ -7,5 +7,7 @@ admin.site.register(User)
 admin.site.register(QuestionType)
 admin.site.register(Question)
 admin.site.register(QuestionChoices)
+admin.site.register(Company)
+admin.site.register(StudentAnswer)
 
 
