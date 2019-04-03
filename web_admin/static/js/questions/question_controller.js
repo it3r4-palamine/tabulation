6bcedd1fa687b7cf2681aaadbf558ff26956aa1b
Albert Vince Mendiola <vinzmendiola@gmail.com>
2019-04-03 14:15:49 +0800

    Yun

--- web_admin/static/js/questions/question_controller.js
+++ web_admin/static/js/questions/question_controller.js
@@ -128,4 +128,41 @@ app.controller('QuestionCtrl', function($scope, $http, $timeout, $element, $cont
 	CommonRead.get_subjects(self);
 
 	self.main_loader();
-});
+})
+
+.directive('icheck', function($timeout)
+{
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function($scope, element, $attrs, ngModel)
+        {
+            return $timeout(function()
+            {
+                var value;
+                value = $attrs['value'];
+
+                $scope.$watch($attrs['ngModel'], function(newValue){
+                    $(element).iCheck('update');
+                })
+
+                return $(element).iCheck({
+                    checkboxClass: 'icheckbox_square-green',
+                    radioClass: 'iradio_square-green'
+
+                }).on('ifChanged', function(event) {
+                        if ($(element).attr('type') === 'checkbox' && $attrs['ngModel']) {
+                            $scope.$apply(function() {
+                                return ngModel.$setViewValue(event.target.checked);
+                            });
+                        }
+                        if ($(element).attr('type') === 'radio' && $attrs['ngModel']) {
+                            return $scope.$apply(function() {
+                                return ngModel.$setViewValue(value);
+                            });
+                        }
+                    });
+            });
+        }
+    };
+})
