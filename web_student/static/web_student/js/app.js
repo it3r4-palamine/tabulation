cff643053b57230127b345700cf5f1e9949d5093
Albert Vince Mendiola <vinzmendiola@gmail.com>
2019-04-24 13:07:42 +0800

    Refactor code for ICheck Directive

--- web_student/static/web_student/js/app.js
+++ web_student/static/web_student/js/app.js
@@ -14,3 +14,40 @@ app.config(['$httpProvider', '$interpolateProvider', function($httpProvider, $in
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
     $interpolateProvider.startSymbol('{$').endSymbol('$}');
 }]);
+
+app.directive('icheck', function($timeout)
+{
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function($scope, element, $attrs, ngModel)
+        {
+            return $timeout(function()
+            {
+                var value;
+                value = $attrs['value'];
+
+                $scope.$watch($attrs['ngModel'], function(newValue){
+                    $(element).iCheck('update');
+                })
+
+                return $(element).iCheck({
+                    checkboxClass: 'icheckbox_square-green',
+                    radioClass: 'iradio_square-green'
+
+                }).on('ifChanged', function(event) {
+                        if ($(element).attr('type') === 'checkbox' && $attrs['ngModel']) {
+                            $scope.$apply(function() {
+                                return ngModel.$setViewValue(event.target.checked);
+                            });
+                        }
+                        if ($(element).attr('type') === 'radio' && $attrs['ngModel']) {
+                            return $scope.$apply(function() {
+                                return ngModel.$setViewValue(value);
+                            });
+                        }
+                    });
+            });
+        }
+    };
+});
